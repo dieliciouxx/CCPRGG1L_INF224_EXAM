05b940eafe8cf7cb4b5da3189033f92677ed4cc3
Ashley Isidoro <111574791+dieliciouxx@users.noreply.github.com>
2022-09-06 09:50:50 +0800

    Create App.java

---
+++ exam_1/App.java
@@ -0,0 +1,26 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+     
+        Student me = new Student();
+
+        me.surname = "isidoro";
+        me.firstName = "ashley gail";
+        me.middleInitial = "l";
+        me.dateOfBirth = "09/10/2003";
+        me.studentNumber = 2022106179;
+        me.EmailAddress = "isidoroal@students.national-u.edu.ph";
+        me.IAmAwesome = true;
+
+        me.sayMysurname();
+        me.sayMyfirstName();
+        me.sayMymiddleInitial();
+        me.sayMydateOfBirth();
+        me.sayMystudentNumber();
+        me.sayMyEmailAddress();
+        me.sayMyIAmAwesome();
+
+
+
+
+    }
+}
