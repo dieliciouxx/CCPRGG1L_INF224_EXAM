b45bfce9cfac6b7303115a3728d754e93bd91c30
Ashley Isidoro <111574791+dieliciouxx@users.noreply.github.com>
2022-09-06 09:52:40 +0800

    Create Student.java

---
+++ exam_1/Student.java
@@ -0,0 +1,30 @@
+public class Student {
+  String surname, firstName, middleInitial,   dateOfBirth, EmailAddress;
+int studentNumber;
+boolean IAmAwesome;
+
+public void sayMysurname(){
+    System.out.println("surname " + surname);
+}
+public void sayMyfirstName(){
+    System.out.println("firstName " + firstName);
+}
+public void sayMymiddleInitial(){
+    System.out.println("middleInitial " + middleInitial);
+}
+public void sayMydateOfBirth(){
+    System.out.println("dateOfBirth " + dateOfBirth);
+}
+public void sayMystudentNumber(){
+    System.out.println("studentNumber " + studentNumber);
+}
+public void sayMyEmailAddress(){
+    System.out.println("EmailAddress " + EmailAddress);
+}
+public void sayMyIAmAwesome(){
+    System.out.println("IAmAwesome " + IAmAwesome);
+}
+
+
+
+}
